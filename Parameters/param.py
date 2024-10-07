3ce2cdd96486a03bda2f0581fb121cee7892734c
santos16Mac <santos16@mcmaster.ca>
2024-10-07 14:14:53 -0400

    Parameter class added

---
+++ Parameters/param.py
@@ -0,0 +1,165 @@
+# This program reads and saves relevant parameters for the pacemaker using a local file. The state of the pacemaker is also stored.
+# Text file formatting: Line 1 = pacemaker state, Line 2 = Lower Rate Limit (LRL), Line 3 = Upper Rate Limit (URL),
+#               Line 4 = Atrial Amplitude (AA), Line 5 = Atrial Pulse Width (APW), Line 6 = Ventricular Amplitude (VA),
+#               Line 7 = Ventricular Pulse Width (VPW), Line 8 = VRP, Line 9 = ARP
+# Extra Parameters possibly Atrial Sensitivity, PVARP, Hysteresis, Rate smoothing, Ventricular Sensitivity
+#               (Can be added upon request for next sprint)
+# Note that states can be either AOO, VOO, AAI,or VVI
+#
+# Author: Serena Santos
+# Date: October 2024
+#
+# Function List:
+#        - __init__(self): Constructor
+#        - save_param(self): Saves current parameters to local file
+#        - load_param(self): Loads parameters from local file to member variables
+
+#        - get_state(self): getter; returns state
+#        - get_LowerRateLimit(self): getter; returns LRL
+#        - get_UpperRateLimit(self): getter; returns URL
+#        - get_AtrialAmplitude(self): getter; returns AA
+#        - get_AtrialPulseWidth(self): getter; returns APW
+#        - get_VentricularAmplitude(self): getter; returns VA
+#        - get_VentricularPulseWidth(self): getter; returns VPW
+#        - get_VRP(self): getter; returns VRP
+#        - get_ARP(self): getter; returns ARP
+
+#        - set_state(self, state): setter; sets state and saves to file
+#        - set_LowerRateLimit(self, new_LRL): setter; sets LRL and saves to file
+#        - set_UpperRateLimit(self, new_URL): setter; sets URL and saves to file
+#        - set_AtrialAmplitude(self, new_AA): setter; sets AA and saves to file
+#        - set_AtrialPulseWidth(self, new_APW): setter; sets APW and saves to file
+#        - set_VentricularAmplitude(self, new_VA): setter; sets VA and saves to file
+#        - set_VentricularPulseWidth(self, new_VPW): setter; sets VPW and saves to file
+#        - set_VRP(self, new_VRP): setter; sets VRP and saves to file
+#        - set_ARP(self, new_ARP): setter; sets ARP and saves to file
+#
+class param:
+    #initialize member variables
+    file_name = "param.txt"
+    state = ''
+    LRL = '0'
+    URL = '0'
+    AA = '0'
+    APW = '0'
+    VA = '0'
+    VPW = '0'
+    VRP = '0'
+    ARP = '0'
+        
+    def __init__(self):
+        #load current values from the file
+        self.load_param()
+        
+    
+    def save_param(self):
+        #open text file in writing mode
+        f = open(self.file_name, "w")
+        f.write(self.state)
+        f.write("\n")
+        f.write(str(self.LRL))
+        f.write("\n")
+        f.write(str(self.URL))
+        f.write("\n")
+        f.write(str(self.AA))
+        f.write("\n")
+        f.write(str(self.APW))
+        f.write("\n")
+        f.write(str(self.VA))
+        f.write("\n")
+        f.write(str(self.VPW))
+        f.write("\n")
+        f.write(str(self.VRP))
+        f.write("\n")
+        f.write(str(self.ARP))
+        f.write("\n")
+        
+        f.close()
+        
+        
+    def load_param(self):
+        #open text file in reading mode
+        f = open(self.file_name, "r")
+        
+        #save each line to its respective variable
+        self.state = f.readline()
+        self.LRL = f.readline()
+        self.URL = f.readline()
+        self.AA = f.readline()
+        self.APW = f.readline()
+        self.VA = f.readline()
+        self.VPW = f.readline()
+        self.VRP = f.readline()
+        self.ARP = f.readline()
+        
+        f.close()
+        
+        
+    
+    #-----------------------getters----------------------
+    def get_state(self):
+        return self.state
+    
+    def get_LowerRateLimit(self):
+        return self.LRL
+    
+    def get_UpperRateLimit(self):
+        return self.URL
+    
+    def get_AtrialAmplitude(self):
+        return self.AA
+    
+    def get_AtrialPulseWidth(self):
+        return self.APW
+    
+    def get_VentricalAmplitude(self):
+        return self.VA
+    
+    def get_VentricalPulseWidth(self):
+        return self.VPW
+    
+    def get_VRP(self):
+        return self.VRP
+        
+    def get_ARP(self):
+        return self.ARP
+    
+    #-----------------setters-----------------------
+    #save data to member variables and local file
+    def set_state(self, new_state):
+        self.state = new_state
+        self.save_param()
+    
+    def set_LowerRateLimit(self, new_LRL):
+        self.LRL = new_LRL
+        self.save_param()
+    
+    def set_UpperRateLimit(self, new_URL):
+        self.URL = new_URL
+        self.save_param()
+    
+    def set_AtrialAmplitude(self, new_AA):
+        self.AA = new_AA
+        self.save_param()
+    
+    def set_AtrialPulseWidth(self, new_APW):
+        self.APW = new_APW
+        self.save_param()
+    
+    def set_VentricalAmplitude(self, new_VA):
+        self.VA = new_VA
+        self.save_param()
+    
+    def set_VentricalPulseWidth(self, new_VPW):
+        self.VPW = new_VPW
+        self.save_param()
+    
+    def set_VRP(self, new_VRP):
+        self.VRP = new_VRP
+        self.save_param()
+        
+    def set_ARP(self, new_ARP):
+        self.ARP = new_ARP
+        self.save_param()
+    
+    
---
+++ Parameters/paramTest.py
@@ -0,0 +1,51 @@
+#Testing for param.
+import param
+
+#create instance of the param object
+parameters = param.param()
+
+# Testing loading parameters from the file and using the getter functions
+print("-----------INITIAL VALUES------------")
+print("State: " + parameters.get_state())
+print("Lower Rate Limit: " + str(parameters.get_LowerRateLimit()))
+print("Upper Rate Limit: " + str(parameters.get_UpperRateLimit()))
+print("Atrial Amplitude: " + str(parameters.get_AtrialAmplitude()))
+print("Atrial Pulse Width: " + str(parameters.get_AtrialPulseWidth()))
+print("Ventrical Amplitude: " + str(parameters.get_VentricalAmplitude()))
+print("Ventrical Pulse Width: " + str(parameters.get_VentricalPulseWidth()))
+print("VRP: " + str(parameters.get_VRP()))
+print("ARP: " + str(parameters.get_ARP()))
+
+#Testing setters and save_param() function(change parameters)
+parameters.set_state("AOO")
+parameters.set_LowerRateLimit(20)
+parameters.set_UpperRateLimit(19)
+parameters.set_AtrialAmplitude(18)
+parameters.set_AtrialPulseWidth(17)
+parameters.set_VentricalAmplitude(16)
+parameters.set_VentricalPulseWidth(15)
+parameters.set_VRP(14)
+parameters.set_ARP(13)
+
+#Print out current values
+print("--------AFTER CHANGES------------")
+print("State: " + parameters.get_state())
+print("Lower Rate Limit: " + str(parameters.get_LowerRateLimit()))
+print("Upper Rate Limit: " + str(parameters.get_UpperRateLimit()))
+print("Atrial Amplitude: " + str(parameters.get_AtrialAmplitude()))
+print("Atrial Pulse Width: " + str(parameters.get_AtrialPulseWidth()))
+print("Ventrical Amplitude: " + str(parameters.get_VentricalAmplitude()))
+print("Ventrical Pulse Width: " + str(parameters.get_VentricalPulseWidth()))
+print("VRP: " + str(parameters.get_VRP()))
+print("ARP: " + str(parameters.get_ARP()))
+
+#Reset to default values if running testing shell again
+parameters.set_state("VOO")
+parameters.set_LowerRateLimit(1)
+parameters.set_UpperRateLimit(2)
+parameters.set_AtrialAmplitude(3)
+parameters.set_AtrialPulseWidth(4)
+parameters.set_VentricalAmplitude(5)
+parameters.set_VentricalPulseWidth(6)
+parameters.set_VRP(7)
+parameters.set_ARP(8)
