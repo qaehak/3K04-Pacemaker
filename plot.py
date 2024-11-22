0c1a97e2c6dbef763284a3ea38035b84e3325b7b
santos16Mac <santos16@mcmaster.ca>
2024-11-22 11:26:10 -0500

    plot

--- plot.py
+++ plot.py
@@ -5,16 +5,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #create data frame
-df = pd.DataFrame({'x': [0,1,2,3],
-                   'y': [0,1,2,3]})
+#df = pd.DataFrame({'x': [0,1,2,3],
+#                   'y': [0,1,2,3]})
 
+# with open("test_data.txt", 'r') as f:
+#     lines = f.readlines()
+#     x = [int(line.split()[0]) for line in lines]
+#     y = [int(line.split()[1]) for line in lines]
+#     df = pd.DataFrame({x,y})
+
+df = pd.read_csv("test_data.txt",sep='\s+',header=None)
+df = pd.DataFrame(df)
+
+x1 = df[0]
+y1 = df[1]
 #set up seaborn
 sns.set()
 
 #set axis
-ax = sns.lineplot(data=df, x='x',y='y')
+ax = sns.lineplot(data=df, x=x1,y=y1)
+print(x1)
+print(y1)
 
 plt.title("Test")
+
+#save plot as png
+plt.savefig("testplot.png")
 
 #display plot
 plt.show()
